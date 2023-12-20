4528edf33d549aff703d5f80a2b6607a70f62eaf
spencerckhuang <shuan126@jhu.edu>
2023-12-20 16:35:06 -0500

    tutorial: finished creating basic controller with PUT and DELETE methods

--- ptri-backend/src/main/java/com/spence/springbootstarter/topic/TopicService.java
+++ ptri-backend/src/main/java/com/spence/springbootstarter/topic/TopicService.java
@@ -1,5 +1,6 @@
 package com.spence.springbootstarter.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,10 +8,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
-    private List<Topic> topics = Arrays.asList(
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
             new Topic("spring", "Spring Framework", "learn about spring framework and springboot"),
             new Topic("algo", "Intro Algorithms", "basic algorithms"),
-            new Topic("lade", "Linear Algebra and Differential Equations", "exactly what it sounds like"));
+            new Topic("lade", "Linear Algebra and Differential Equations", "exactly what it sounds like")));
 
     public List<Topic> getAllTopics() {
         return topics;
@@ -19,5 +20,23 @@ public class TopicService {
     // Iterate over list of topics and find first Topic that matches Id
     public Topic getTopic(String id) {
         return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+
+    public void updateTopic(String id, Topic topic) {
+        for (int i = 0; i < topics.size(); i++) {
+            Topic t = topics.get(i);
+            if (t.getId().equals(id)) {
+                topics.set(i, topic);
+                return;
+            }
+        }
+    }
+
+    public void deleteTopic(String id) {
+        topics.removeIf(t -> t.getId().equals(id));
     }
 }
