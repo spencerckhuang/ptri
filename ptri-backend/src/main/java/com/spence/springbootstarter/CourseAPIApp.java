e9a482e7fab63c2dd3a9cc7eb8dac2dae0911380
spencerckhuang <shuan126@jhu.edu>
2023-12-20 02:03:14 -0500

    first commit: init springboot app

---
+++ ptri-backend/src/main/java/com/spence/springbootstarter/CourseAPIApp.java
@@ -0,0 +1,11 @@
+package com.spence.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseAPIApp {
+    public static void main(String[] args) {
+        SpringApplication.run(CourseAPIApp.class, args);
+    }
+}
---
+++ ptri-backend/src/test/java/com/spence/ptribackend/PtriBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spence.ptribackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtriBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
