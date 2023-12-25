b86d42c8ab6605eee622457712415e4bf39d2a22
spencerckhuang <shuan126@jhu.edu>
2023-12-25 11:22:45 -0500

    refactored to start init process on app startup

--- ptri-backend/src/main/java/com/spence/jhucourse/CourseAPI.java
+++ ptri-backend/src/main/java/com/spence/jhucourse/CourseAPI.java
@@ -1,11 +1,24 @@
 package com.spence.jhucourse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import com.spence.jhucourse.course.CourseService;
 
 @SpringBootApplication
 public class CourseAPI {
+    @Autowired
+    private CourseService courseService;
+
     public static void main(String[] args) {
         SpringApplication.run(CourseAPI.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void init() {
+        courseService.getAllCourses();
     }
 }
