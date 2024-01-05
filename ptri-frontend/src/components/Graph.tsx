8bd5b50af20ec515ee97e976936dcb8a8c1925c2
spencerckhuang <shuan126@jh.edu>
2024-01-05 11:57:46 -0500

    added axios GET request template, added hard-coded testing nodes

--- ptri-frontend/src/components/Graph.tsx
+++ ptri-frontend/src/components/Graph.tsx
@@ -1,41 +1,91 @@
 import React, { useCallback } from 'react';
 import ReactFlow, { useNodesState, useEdgesState, addEdge } from 'reactflow';
 import 'reactflow/dist/style.css';
+import axios from 'axios';
 
-const getNodes = () => {
+const BASE_URL = "localhost:8080/";
+
+type CourseNode = {
+    id: string;
+    position: {
+        x: number;
+        y: number;
+    };
+    data: {
+        label: string;
+        offeringName: string;
+    };
+}
+
+type CourseEdge = {
+    id: string;
+    source: string;
+    target: string;
+}
+
+const generateNodesAndEdges = () => {
     // JSON object-like structure
     /*
         id
         position: {<x coord>, <y coord>}
         data: {label: 'one', <more data>}
     */
+
+    // ! Just for testing purposes
+    const initialNodes: CourseNode[] = [
+        { id: 'EN.601.226', position: { x: 200, y: 200 }, data: { 
+            label: 'Data Structures',
+            offeringName: 'EN.601.226'
+        }},
+        { id: 'EN.601.220', position: { x: 0, y: 0 }, data: { 
+            label: 'Intermediate Programming',
+            offeringName: 'EN.601.220'
+        }},
+        { id: 'EN.500.132', position: { x: 0, y: 200 }, data: { 
+            label: 'Bootcamp: Java',
+            offeringName: 'EN.500.132'
+        }},
+        { id: 'EN.500.112', position: { x: 0, y: 400 }, data: { 
+            label: 'Gateway Computing: JAVA',
+            offeringName: 'EN.500.112'
+        }},
+    
+    ];
+
+    // ! Just for testing purposes
+    const initialEdges: CourseEdge[] = [
+        { id: 'e1', source: 'EN.601.220', target: 'EN.601.226' },
+        { id: 'e2', source: 'EN.500.132', target: 'EN.601.226'},
+        { id: 'e3', source: 'EN.500.112', target: 'EN.601.226'}
+
+    ];
+   
+    
+    axios.get(BASE_URL + "courses")
+        .then(function (response) {
+            // Create all nodes
+
+
+            // Create all edges
+        })
+        .catch(function (error) {
+
+        }).finally(function () {
+
+        });
+    
     return (
-        [
-            { id: '1', position: { x: 0, y: 0 }, data: { label: 'one' } },
-            { id: '2', position: { x: 0, y: 100 }, data: { label: 'two' } },
-            { id: '3', position: { x: 0, y: 200 }, data: { label: 'three' } },
-        ]
+        [initialNodes, initialEdges]
     );
 }
 
-const getEdges = () => {
-    /*
-        id
-        source: id of source node
-        target: id of target node
-    */
-    return (
-        [
-            { id: 'e1-2', source: '1', target: '2' },
-            { id: 'e2-3', source: '2', target: '3'}
-        ]
-    );
-}
 
 
 const Graph = () => {
-    const initialNodes = getNodes();
-    const initialEdges = getEdges();
+    const nodesAndEdges = generateNodesAndEdges();
+
+    const initialNodes = nodesAndEdges[0];
+    const initialEdges = nodesAndEdges[1];
 
     const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
     const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
